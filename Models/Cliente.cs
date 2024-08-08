693db669fcb4688b7a78e043e70367e96a85881b
Mikael Amaral <mikael.a.amaral07@gmail.com>
2024-08-08 16:52:06 -0300

    Adiciondo o Model 'Cliente' e a view 'CadastroCliente'. (GitGui)

---
+++ Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿namespace ExercicioGitHubMarqueto.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string CPF { get; set; } = string.Empty ;
+        public string RG { get; set; } = string.Empty;
+        public string DataNascimento { get; set; } = string.Empty;
+        public string CEP { get; set; } = string.Empty;
+        public string UF { get; set; } = string.Empty;
+        public string Cidade { get; set; } = string.Empty;
+        public string Rua { get; set; } = string.Empty;
+        public string Numero { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Celular { get; set; } = string.Empty;
+    }
+}
