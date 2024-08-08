1d87f51467c4fb3e142013d602bb7a88241b962e
Mikael Amaral <mikael.a.amaral07@gmail.com>
2024-08-08 16:37:10 -0300

    Subindo o layout com o css. (GitGui)

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExercicioGitHubMarqueto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
