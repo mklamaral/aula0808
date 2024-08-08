646ab01b46b9da1621267050a06c0879d0d80263
Mikael Amaral <mikael.a.amaral07@gmail.com>
2024-08-08 16:47:09 -0300

    adicionando o Controller Cliente e a AulaContext para o DbSet. (GitGui)

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ExercicioGitHubMarqueto.Data;
+
+namespace ExercicioGitHubMarqueto.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly AulaContext _context;
+
+        public ClienteController(AulaContext context) 
+        {
+            _context = context;
+        }
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CadastroCliente([Bind("Id,Nome,RG,CPF,Usuario,Senha,CEP,UF,Cidade,Bairro,Rua,Numero,Complemento")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("BuscaCliente");
+            }
+            return View(cliente);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/AulaContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ExercicioGitHubMarqueto.Data
+{
+    public class AulaContext : DbContext
+    {
+        public AulaContext(DbContextOptions<AulaContext> options) : base(options) 
+        {
+        }
+    }
+}
